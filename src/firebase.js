e3e678936ac298fdb1c5fc6de14ae4b4d808e31a
Jan Sularz <72702422+Jachu7@users.noreply.github.com>
2023-06-16 15:28:47 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,14 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAq6xuAO6i2DFvqSCKv9O-3MXN7oH6eocw",
-    authDomain: "projekt-6-ee0ec.firebaseapp.com",
-    projectId: "projekt-6-ee0ec",
-    storageBucket: "projekt-6-ee0ec.appspot.com",
-    messagingSenderId: "48165191539",
-    appId: "1:48165191539:web:d6a2907d08cea2cb12cf06",
-    measurementId: "G-6G2F531HD4",
-    databaseURL:
-        "https://projekt-6-ee0ec-default-rtdb.europe-west1.firebasedatabase.app",
+    apiKey: "...",
+    authDomain: "...",
+    projectId: "...",
+    storageBucket: "...",
+    messagingSenderId: "...",
+    appId: "...",
+    measurementId: "...",
+    databaseURL:"...",
 };
 
 // Initialize Firebase
